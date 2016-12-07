e0840104f5f6c7f30d83f5e9e1ca663d841c2b29
vsuhina <vanja.suhina@gmail.com>
2016-12-07 01:10:00 +0100

    Add files via upload

---
+++ 4/4-1.py
@@ -0,0 +1,41 @@
+import re
+
+f = open(r'C:\Users\Vanja\Desktop\AoC2016\4\input.txt','r')
+
+f.seek(0)
+lines = f.read().splitlines()
+
+pattern = '([a-z\-]+)(\d+)\[([a-z]+)\]'
+
+
+sumIDs = 0
+
+for line in lines:
+	m = re.search(pattern, line)
+	elements = list(m.groups())
+	
+	code = elements[0].replace('-','')
+	id = (int)(elements[1])
+	checksum = elements[2]
+	
+	if isValid(code, checksum):
+		print(id, code, checksum)
+		sumIDs += id
+
+print sumIDs
+	
+def isValid(code, checksum):
+	charCount = []
+	chars = list(set(code))
+	chars.sort()
+	for c in chars:
+		charCount.append([c,code.count(c)])
+		
+	charCount.sort(key= lambda x: (-x[1], x[0]))
+	
+	for i in range(0,5):
+		if (charCount[i][0] != checksum[i]):			
+			return False
+					
+	return True
+	
